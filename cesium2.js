2d12255545730007dba3af21e3ba3fdbea972d8b
syikoo <ookiyasu@hotmail.com>
2023-10-05 03:02:30 +0900

    anim

--- cesium2.js
+++ cesium2.js
@@ -1,10 +1,9 @@
-
 //--------------------------------------------------------------
 // 初期表示
 
-const viewer = new Cesium.Viewer("cesiumContainer", {
-  timeline: false,
-  animation: false,
+let viewer = new Cesium.Viewer("cesiumContainer", {
+  timeline: true,
+  animation: true,
   sceneModePicker: false,
   baseLayerPicker: false,
 });
@@ -36,69 +35,64 @@ viewer.scene.camera.setView({
   ),
 }); 
 
-Sandcastle.addDefaultToolbarButton("log", function () {
-console.log('log');
-});
-
 //--------------------------------------------------------------
 // データロード
 
-const uczml = 'https://syikoo.github.io/gh-pages1/tmp.czml';
+const uczml = 'https://syikoo.github.io/gh-pages1-public/tmp.czml';
 const dataSourcePromise1 = Cesium.CzmlDataSource.load(uczml);
 viewer.dataSources.add(dataSourcePromise1);
 viewer.zoomTo(dataSourcePromise1);
 
 dataSourcePromise1.then(dataSource=>{
-  //--------------------------------------------------------------
-  // UI作成
-  
-  //----------------------------
-  // プルダウンメニュー
-  console.log('log1');
-    
-  // 既存のPolylineエンティティを取得
-  const polylineEntities = dataSource.entities.values.filter(entity => entity.polyline);
-  
-  
-    // プルダウンメニューの作成
-  polylineEntities.forEach(entity => {
-      const option = document.createElement('option');
-      option.value = entity.id;
-      option.textContent = entity.name || entity.id;  // エンティティに名前があればそれを使用、なければIDを使用
-      document.getElementById('lineSelector').appendChild(option);
-  });
-  
-  // 以前に選択されていたPolylineエンティティを保持する変数
-  let previousSelectedLine;
-  let selectedLineId;
-  let selectedLine;
+//--------------------------------------------------------------
+// UI作成
 
-  // プルダウンメニューの選択イベントのリスニング
-  document.getElementById('lineSelector').addEventListener('change', function(event) {
-      selectedLineId = event.target.value;
-      selectedLine = dataSource.entities.getById(selectedLineId);
+//----------------------------
+// プルダウンメニュー
+console.log('log1');
   
-      // 以前に選択されていたPolylineの強調表示を解除
-      if (previousSelectedLine) {
-          previousSelectedLine.polyline.material = Cesium.Color.WHITE;
-          previousSelectedLine.polyline.width = 2;
-          // 開始点・終了点の強調表示を解除する処理もここに追加...
-      }
-  
-      // 新しく選択されたPolylineを強調表示
-      selectedLine.polyline.material = Cesium.Color.RED;
-      selectedLine.polyline.width = 8;
-      // 開始点・終了点の強調表示の処理もここに追加...
-  
-      // 選択されたPolylineをpreviousSelectedLineとして保持
-      previousSelectedLine = selectedLine;
-  });
+// 既存のPolylineエンティティを取得
+const polylineEntities = dataSource.entities.values.filter(entity => entity.polyline);
 
+
+  // プルダウンメニューの作成
+polylineEntities.forEach(entity => {
+    const option = document.createElement('option');
+    option.value = entity.id;
+    option.textContent = entity.name || entity.id;  // エンティティに名前があればそれを使用、なければIDを使用
+    document.getElementById('lineSelector').appendChild(option);
+});
+
+// 以前に選択されていたPolylineエンティティを保持する変数
+let previousSelectedLine;
+let selectedLineId;
+let selectedLine;
+
+// プルダウンメニューの選択イベントのリスニング
+document.getElementById('lineSelector').addEventListener('change', function(event) {
+     selectedLineId = event.target.value;
+     selectedLine = dataSource.entities.getById(selectedLineId);
+
+    // 以前に選択されていたPolylineの強調表示を解除
+    if (previousSelectedLine) {
+        previousSelectedLine.polyline.material = Cesium.Color.WHITE;
+        previousSelectedLine.polyline.width = 2;
+        // 開始点・終了点の強調表示を解除する処理もここに追加...
+    }
+
+    // 新しく選択されたPolylineを強調表示
+    selectedLine.polyline.material = Cesium.Color.RED;
+    selectedLine.polyline.width = 5;
+    // 開始点・終了点の強調表示の処理もここに追加...
+
+    // 選択されたPolylineをpreviousSelectedLineとして保持
+    previousSelectedLine = selectedLine;
+});
   //----------------------------
   // 左クリックイベントを追加
   const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
   handler.setInputAction(function(click) {
-      const pickedObject = dataSource.scene.pick(click.position);
+      const pickedObject = viewer.scene.pick(click.position);
       if (Cesium.defined(pickedObject) && pickedObject.id && pickedObject.id.polyline) {
           const selectedEntity = pickedObject.id;
 
@@ -129,7 +123,7 @@ dataSourcePromise1.then(dataSource=>{
       name: "Blue box",
       position: viewer.scene.camera.position,
       box: {
-        dimensions: new Cesium.Cartesian3(1000.0, 1000.0, 1000.0),
+        dimensions: new Cesium.Cartesian3(10.0, 10.0, 10.0),
         material: Cesium.Color.BLUE,
       },
     });  
@@ -145,30 +139,46 @@ dataSourcePromise1.then(dataSource=>{
     viewer.clock.canAnimate = false;  // アニメーションを開始時にのみ実行
 
     const position = new Cesium.SampledPositionProperty();
-    for (let i = 0; i < polylineData.positions.length; i++) {
+    for (let i = 0; i < polylineData.positions._value.length; i++) {
         const time = Cesium.JulianDate.addSeconds(start, i, new Cesium.JulianDate());
-        const positionValue = polylineData.positions[i];
-        position.addSample(time, positionValue);
+        const positionValue = polylineData.positions._value[i];
+      position.addSample(time, positionValue);
     }
     car.position = position;
     car.orientation = new Cesium.VelocityOrientationProperty(position);
     
-    viewer.clock.multiplier = 10;  // アニメーションのスピードを設定
+    viewer.clock.multiplier = 7;  // アニメーションのスピードを設定
     viewer.clock.canAnimate = true;  // アニメーションを開始
 
     // カメラの移動をアップデート
     viewer.scene.preRender.addEventListener(function(scene, time) {
-        const carPosition = car.position.getValue(time);
-        const offset = Cesium.Cartesian3.fromDegrees(0, 0, 100);  // 100m上空
+      try {
+        let carPosition = car.position.getValue(time);
+       /*
+        const nextTime = Cesium.JulianDate.addSeconds(time, 5, new Cesium.JulianDate());
+        let nextCarPosition = car.position.getValue(nextTime);
+        const direction = Cesium.Cartesian3.subtract(nextCarPosition, carPosition, new Cesium.Cartesian3());
+       */
+        const offset = Cesium.Cartesian3.fromDegrees(0, 0, 400);  // 200m上空
         const cameraPosition = Cesium.Cartesian3.add(carPosition, offset, new Cesium.Cartesian3());
-
-        viewer.camera.lookAt(carPosition, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(-45), Cesium.Math.toRadians(-45), 100));
+        viewer.camera.lookAt(carPosition, new Cesium.HeadingPitchRange(Cesium.Math.toRadians(180), Cesium.Math.toRadians(-20), 500));
+      }
+      catch(err){
+      }
     });
   }
   
-Sandcastle.addDefaultToolbarButton("start", function () {
+  
+document.getElementById("startAnimation").addEventListener('click', function () {
   console.log('log start');
   startAnimation();
+});
+document.getElementById('stopAnimation').addEventListener('click', function() {
+    viewer.clock.shouldAnimate = false;  // アニメーションを停止
+});
+document.getElementById("logButton").addEventListener('click', function () {
+  console.log('log');
+  console.log(viewer);
 });
 
 });
